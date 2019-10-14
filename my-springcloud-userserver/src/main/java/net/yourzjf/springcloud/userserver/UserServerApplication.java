58dc9170b61dd46bc75565f74b2eb45cc21a90ca
zhangjianfeng <zhangjianfeng@hztianque.com>
2019-10-14 10:09:12 +0800

    init

---
+++ my-springcloud-userserver/src/main/java/net/yourzjf/springcloud/userserver/UserServerApplication.java
@@ -0,0 +1,22 @@
+package net.yourzjf.springcloud.userserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class UserServerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return configureApplication(application);
+    }
+
+    public static void main(String[] args) {
+        configureApplication(new SpringApplicationBuilder()).run(args);
+    }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(UserServerApplication.class);
+    }
+}
